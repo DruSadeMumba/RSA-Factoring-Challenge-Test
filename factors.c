f9b3556ee5c2d940330429c1fadc8bd5362c397d
Dru Mumba <100194473+DruSadeMumba@users.noreply.github.com>
2023-09-03 14:12:58 +0300

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * factorize - find factors of int
+ * @n: input int
+ */
 void factorize(long long n) {
     long long i = 2;
-    while (i * i <= n) {
-        if (n % i == 0) {
+    while (i * i <= n)
+    {
+        if (n % i == 0)
+        {
             printf("%lld=%lld*%lld\n", n, n / i, i);
             return;
         }
@@ -13,21 +19,33 @@ void factorize(long long n) {
     printf("%lld is a prime number.\n", n);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+/**
+ * main - main func
+ * @argc: num of args
+ * @argv: vector
+ * Return: 0 success or 1 failure
+ */
+int main(int argc, char *argv[]) 
+{
+    FILE *file = fopen(argv[1], "r");
+    char buffer[256];
+    long long num;
+    
+    if (argc != 2) 
+    {
         fprintf(stderr, "Usage: ./factors <testfile>\n");
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Error opening the file");
         return 1;
     }
 
-    char buffer[256];
-    while (fgets(buffer, sizeof(buffer), file)) {
-        long long num = atoll(buffer);
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        num = atoll(buffer);
         factorize(num);
     }
 
