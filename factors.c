844e969a3b08f6153b44da0400e68a8ed6f2d932
Dru Mumba <100194473+DruSadeMumba@users.noreply.github.com>
2023-09-03 14:20:44 +0300

    Update factors.c

--- factors.c
+++ factors.c
@@ -28,7 +28,7 @@ void factorize(long long n) {
 int main(int argc, char *argv[]) 
 {
     FILE *file = fopen(argv[1], "r");
-    char buffer[256], data;
+    char buffer[256];
     long long num;
     
     if (argc != 2) 
